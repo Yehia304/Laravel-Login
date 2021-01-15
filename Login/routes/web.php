ce4387a43e24766e1b2541d8816d5a234236907a
Yehia El Sayed <yehiafml20333@gmail.com>
2021-01-15 19:52:43 +0200

    User authentication

--- Login/routes/web.php
+++ Login/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    if(Auth::check()){
+
+        return 'User is logged in';
+
+    }
     return view('welcome');
 });
 
